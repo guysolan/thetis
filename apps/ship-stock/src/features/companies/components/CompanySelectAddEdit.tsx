0700662262bdfaa7ecbd55b713c551d231d9fa5e
Guy_Solan <guysolan202@gmail.com>
2024-12-09 11:11:18 +0100

    company contact form

--- apps/ship-stock/src/features/companies/components/CompanySelectAddEdit.tsx
+++ apps/ship-stock/src/features/companies/components/CompanySelectAddEdit.tsx
@@ -6,44 +6,38 @@ import TooltipDialog from "@/components/TooltipDialog";
 import CompanyForm from "./CompanyForm";
 
 interface Props {
-    name: string;
+  name: string;
 }
 
 const CompanySelectAddEdit = ({ name }: Props) => {
-    const form = useFormContext();
-    const { data: companies = [] } = useSelectCompanies();
+  const form = useFormContext();
+  const { data: companies = [] } = useSelectCompanies();
 
-    const getSelectedCompany = () => {
-        const companyId = form.watch(name);
-        return companies?.find((c) => String(c.id) === companyId);
-    };
+  const getSelectedCompany = () => {
+    const companyId = form.watch(name);
+    return companies?.find((c) => String(c.id) === companyId);
+  };
 
-    return (
-        <div className="flex flex-row items-end gap-2 w-full">
-            <Select
-                label="Company"
-                name={name}
-                options={(companies || []).map((c) => ({
-                    label: c.name,
-                    value: String(c.id),
-                }))}
-            />
-            {getSelectedCompany() && (
-                <TooltipDialog
-                    icon={<Pencil size={20} />}
-                    tooltipText={"Edit Company"}
-                >
-                    <CompanyForm company={getSelectedCompany()} />
-                </TooltipDialog>
-            )}
-            <TooltipDialog
-                icon={<Plus size={20} />}
-                tooltipText={"Add Company"}
-            >
-                <CompanyForm />
-            </TooltipDialog>
-        </div>
-    );
+  return (
+    <div className="flex flex-row items-end gap-2 w-full">
+      <Select
+        label="Company"
+        name={name}
+        options={(companies || []).map((c) => ({
+          label: c.name,
+          value: String(c.id),
+        }))}
+      />
+      {getSelectedCompany() && (
+        <TooltipDialog icon={<Pencil size={20} />} tooltipText={"Edit Company"}>
+          <CompanyForm company={getSelectedCompany()} />
+        </TooltipDialog>
+      )}
+      <TooltipDialog icon={<Plus size={20} />} tooltipText={"Add Company"}>
+        <CompanyForm />
+      </TooltipDialog>
+    </div>
+  );
 };
 
 export default CompanySelectAddEdit;
