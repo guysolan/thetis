ae5af0c8714c0b29b168d40d8a08e259542b29f5
Guy_Solan <guysolan202@gmail.com>
2024-12-13 23:21:15 +0100

    vip badges

--- apps/website/src/data/testimonials.ts
+++ apps/website/src/data/testimonials.ts
@@ -5,31 +5,42 @@ import KimDaybell from "../assets/faces/kim-daybell.png";
 
 export const testimonials = [
   {
+    is_pinned: true,
     name: "Andrew Lawrence",
     description: "Team GB Basketball Player",
     image: AndrewLawrence,
+    short:
+      "Revolutionary night splint - comfortable sleep with no fear of injury",
     quote:
       "I recently began using the Thetis Night Splint overnight. My regular boot is good for daily use but very heavy and uncomfortable to sleep in - the night splint has been revolutionary for me! It keeps my foot perfectly in place but without being bulky, meaning I can sleep more comfortably with no fear of hurting my Achilles tendon. I would highly recommend everyone and anyone to use this product during your recovery from Achilles rupture.",
   },
   {
+    is_pinned: true,
     name: "Olivia Blatch",
     description: "Team GB Weightlifter",
     image: OliviaBlatch,
+    short:
+      "Slimmer, lightweight, and allows free movement while protecting the tendon",
     quote:
       "The Thetis Night Splint is a lot slimmer and far more lightweight than the orthopaedic boot I was sleeping in. It allows me to move my leg more freely but still stops me from flexing my toes beyond a certain point, which is ideal whilst the tendon heals. The splint even comes in my favourite colour, so I was sold the minute I saw it!",
   },
   {
+    is_pinned: false,
     name: "Mr James Davis",
     description:
       "Past President of the British Orthopaedic Foot and Ankle Society",
     image: MrJamesDavis,
+    short:
+      "A fantastic alternative to uncomfortable hospital boots for night wear",
     quote:
       "When I tore my own Achilles tendon, the hardest part of the recovery was having to wear the hospital boot in bed at night for many weeks. My only option was to make myself a splint using plaster-cast materials and make-shift straps. It is fantastic that Thetis Medical have produced this night-splint. It is certain to improve the recovery experience for patients.",
   },
   {
+    is_pinned: true,
     name: "Kim Daybell",
     description: "Team GB Table Tennis and Doctor",
     image: KimDaybell,
+    short: "Simple design that allows better sleep and wound breathing",
     quote:
       "The Thetis Night Splint has a simple design that is perfectly fit for purpose. It has allowed me to get the sleep that is so vital as I recovered from achilles tendon rupture and surgery whilst also feeling secure and protected. Apart from being less bulky than the orthopaedic boot, it also gives the surgical wounds a chance to breath and has made the recovery period a lot more bearable. I would highly recommend this product!",
   },
