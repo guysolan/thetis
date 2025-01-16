282f8294ac90f82ef1a6ddafdca8e1d66e03082a
Guy_Solan <guysolan202@gmail.com>
2025-01-16 21:02:09 +0100

    hows that?

--- apps/website/tailwind.config.ts
+++ apps/website/tailwind.config.ts
@@ -3,10 +3,6 @@ import baseConfig from "@thetis/ui/tailwind.config";
 export default {
   content: [
     "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
   ],
   darkMode: "class",
   presets: [baseConfig],
