c74ee5af3372c819a88279c995873405ea764778
Guy_Solan <guysolan202@gmail.com>
2025-04-13 22:34:36 +0100

    utils fix

--- apps/ship-stock/src/features/orders/features/order-documents/utils/utils.ts
+++ apps/ship-stock/src/features/orders/features/order-documents/utils/utils.ts
@@ -1,22 +1,18 @@
 import { OrderView } from "../../../types";
 
-export const prepareOrderItems = (order: OrderView, noNegatives = false) => {
-    const noPackagesOrServices = order.items.filter((item) =>
+export const prepareOrderItems = (order: OrderView) => {
+    const noPackagesOrServices = order.items?.filter((item) =>
         item.item_type !== "package" && item.item_type !== "service"
     );
 
-    if (noNegatives) {
-        return noPackagesOrServices.filter((item) => item.quantity > 0);
-    }
-
     if (order.order_type === "sale") {
-        return noPackagesOrServices.map((item) => ({
+        return noPackagesOrServices?.map((item) => ({
             ...item,
             quantity: Math.abs(item.quantity),
         }));
     }
     if (order.order_type === "purchase") {
-        return noNegatives;
+        return noPackagesOrServices?.filter((item) => item.quantity > 0);
     }
     return noPackagesOrServices;
 };
